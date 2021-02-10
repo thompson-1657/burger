a6879466fdbbc0d198307eb79696e8d9e1da0078
Rachel Thompson <thompson.1657@gmail.com>
2021-02-10 17:05:41 -0600

    created burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bluecheese Bacon Burger');
