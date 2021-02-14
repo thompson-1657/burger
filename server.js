defe2eacf11b45285163019746a4a9775234c3b9
Rachel Thompson <thompson.1657@gmail.com>
2021-02-14 11:09:35 -0600

    basic css/ index.hanglebars layout

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
