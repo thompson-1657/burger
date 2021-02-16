a82e51f87cf61388ebbc5634e0ede0c16b55358a
Rachel Thompson <thompson.1657@gmail.com>
2021-02-16 12:52:21 -0600

    readded port

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
   connection = mysql.createConnection({
+    port: 3306,
     host:'localhost',
     user:'root',
     password:'Rootroot23',
