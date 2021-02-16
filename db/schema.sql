b55cd4252ea4a3ab00027d80bea7258c70e43295
Rachel Thompson <thompson.1657@gmail.com>
2021-02-16 12:03:50 -0600

    changed main to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
